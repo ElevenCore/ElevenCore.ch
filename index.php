defcdd902d14f51ff140a2255735459848220666
ElevenCore <alejandro.probst@gmx.ch>
2018-02-14 22:16:14 +0100

    Added gitignore

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="css/styles.css" type="text/css">
-        <script src="jquery-3.3.1.min.js"></script>
+        <script src="js/jquery-3.3.1.min.js"></script>
     </head>
     <body>
         <!-- Container for the whole Website -->
